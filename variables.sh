5d44ab69705629cc09a47a958cb5fd188700bc5d
surendra-karamsetty <surendra3207@gmail.com>
2026-01-28 19:33:25 +0530

    Commit changes

--- variables.sh
+++ variables.sh
@@ -3,3 +3,11 @@
 echo "Please enter you user name :: $USER_NAME"
 
 read USER_NMAE
+
+echo "user name is $USER_NAME"
+
+echo "Please enter your password"
+
+read -s PASS_WORD
+
+echo "Check your passwprd $PASS_WORD"
