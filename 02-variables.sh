0b5502fee2084f451dd37acb16f5cad3b7620584
surendra-karamsetty <surendra3207@gmail.com>
2026-01-28 20:59:05 +0530

    Commit changes

--- 02-variables.sh
+++ 02-variables.sh
@@ -3,7 +3,7 @@
 echo "All variables passed to the script $@"
 echo "Number of variables passed to the script $#"
 echo "script name $0"
-echo "Present which directory you are in $PWD"
+echo "Present working directory $PWD"
 echo "who is running the script $USER"
 echo "Home directory of the user $HOME"
 echo "PID of the script $$"
