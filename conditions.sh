5972e810bcc04155b02243cb53586669afc9df8a
surendra-karamsetty <surendra3207@gmail.com>
2026-01-28 21:50:22 +0530

    Commit changes

--- conditions.sh
+++ conditions.sh
@@ -3,9 +3,9 @@
 NUMBER=$1
 
 if [$NUMBER -gt 20 ]; then
-echo "Given number is $NUMBER is grater than 20"
+    echo "Given number  $NUMBER is grater than 20"
 elif [$NUMBER -eq 20]; then
-echo "Gien number is $NUMBER is equal to 20"
+    echo "Gien number  $NUMBER is equal to 20"
 elif [$NUMBER -lt 20]; then
-echo "Given number is $NUMBER is less than 20"
+    echo "Given number  $NUMBER is less than 20"
 fi
