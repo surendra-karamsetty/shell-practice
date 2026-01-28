8e8e66c7c89f3a244121fee988df6117d531f66f
surendra-karamsetty <surendra3207@gmail.com>
2026-01-28 22:30:04 +0530

    Commit changes

--- Instalation.sh
+++ Instalation.sh
@@ -9,6 +9,20 @@ fi
 
 echo "You have executed with root user so installing nginx"
 
+START_TIME=$(date +%s)
+
+echo "Script start time $START_TIME"
+
 dnf install nginx -y
 
-echo "nginx installation is done."
+if [ $? is -eq 0 ]; then
+
+echo "nginx installation is done"
+
+END_TIME=$(date +%s)
+
+echo "Script end time $END_TIME"
+
+TOTAL_TIME=((END_TIME-START_TIME))
+
+echo "Total time taken to install the nginx $TOTAL_TIME"
