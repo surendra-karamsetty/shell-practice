f8acdcc98501e401d44261be47bad2cf9e5b2b4d
surendra-karamsetty <surendra3207@gmail.com>
2026-01-29 20:50:46 +0530

    Commit Changes

--- loops.sh
+++ loops.sh
@@ -4,17 +4,17 @@ USERID=$(id -u)
 LOG_FOLDER="/var/log/shell-script"
 LOG_FILE="/var/log/shell-script/$0.log"
 
-if ( $USERID -ne 0 ); then
+if [ $USERID -ne 0 ]; then
     echo "Please run the script with sudo user"
     exit 1
 else
     echo "Script was executed with sudo user hence installing the package"
 fi
 
-mkdie -p $LOG_FOLDER
+mkdir -p $LOG_FOLDER
 
 VALIDATION(){
-    if ( $1 -ne 0 ); then
+    if [ $1 -ne 0 ]; then
         echo "$2 ..FAILED"
         exit 1
     else
