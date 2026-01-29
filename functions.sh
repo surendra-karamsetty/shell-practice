20374065c6215fe79c399a3ac1f3b23fd3044e64
surendra-karamsetty <surendra3207@gmail.com>
2026-01-29 19:36:42 +0530

    Commit Changes

--- functions.sh
+++ functions.sh
@@ -2,15 +2,15 @@
 
 USERID=$(id -u)
 
-if [ $USERID -nq 0 ]; then
-    echo "Please run the script woth sudo user"
+if [ $USERID -ne 0 ]; then
+    echo "Please run the script with sudo user"
     exit 1
 else
     echo "Executed the script with sudo user hence installing the nginx"
 fi
 
 VALIDATION(){
-    if [ $1 -nq 0 ]; then
+    if [ $1 -ne 0 ]; then
         echo "$2 .. FAILED"
         exit 1
     else
