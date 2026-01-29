07b3f83b50b265dcd958b357f332264c62d87470
surendra-karamsetty <surendra3207@gmail.com>
2026-01-29 21:53:12 +0530

    Commit Changes

---
+++ set.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -e
+
+echo "My name is surendra"
+echo "I am learing DevOps as well"
+echoo "I am leaning unix and shell scripting"
+echo "I ahve completed my basic shell command"
---
+++ trap.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+set -e
+
+trap 'echo "There is an error in line $LINEND, Command: $BASH_COMMAND"' ERR
+
+echo "My name is surendra"
+echo "I am learing DevOps as well"
+echoo "I am leaning unix and shell scripting"
+echo "I ahve completed my basic shell command"
