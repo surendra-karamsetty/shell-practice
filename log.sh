8ed05e712879747a6f9d300ba2b1a04eb1a54c36
surendra-karamsetty <surendra3207@gmail.com>
2026-01-29 20:09:02 +0530

    Commit Changes

--- log.sh
+++ log.sh
@@ -4,7 +4,7 @@ USERID=$(id -u)
 LOG_FOLDER="/var/log/shell-script"
 LOG_FILE="/var/log/shell-script/$0.log"
 
-if [ USERID -ne 0 ]; then
+if [ $USERID -ne 0 ]; then
     echo "Please execute the script with sudo user"
     exit 1
 else
@@ -25,5 +25,5 @@ VALIDATION(){
 dnf install nginx -y &>> LOG_FILE
 VALIDATION $? "Installing nginx"
 
-dnf install -mysql -y &>> LOG_FILE
+dnf install mysql -y &>> LOG_FILE
 VALIDATION $? "Installing mysql"
